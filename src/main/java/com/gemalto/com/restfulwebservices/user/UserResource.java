e0eb9900eaf1e032ce45bb72356a5b20799c10f7
archit_chandra <send2archit.chandra@gmail.com>
2017-12-31 05:41:48 +0100

    Added HATEOS resource

--- src/main/java/com/gemalto/com/restfulwebservices/user/UserResource.java
+++ src/main/java/com/gemalto/com/restfulwebservices/user/UserResource.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,14 +35,21 @@ public class UserResource {
     // retrieve a specific user
     // (GET /users/{id})
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public Resource<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
         
         if (user == null) {
             throw new UserNotFoundException("id="+id);
         }
         
-        return user;
+        // "all-users", SERVER_PATH +"/users" --> Bad Approach
+        // "all-users", retrieveAllUsers()    --> Good Approach
+        Resource<User> resource = new Resource<User>(user);
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+        return resource;
+        
+        //Returned HATEOAS resource
     }
     
     // add a user
