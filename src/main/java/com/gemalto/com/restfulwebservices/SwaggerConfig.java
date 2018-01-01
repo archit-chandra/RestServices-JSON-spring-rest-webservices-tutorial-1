d6acfb5647eddb951988f5052a00d587e8a48e3d
archit_chandra <send2archit.chandra@gmail.com>
2018-01-01 03:20:45 +0100

    objectives defined

---
+++ src/main/java/com/gemalto/com/restfulwebservices/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.gemalto.com.restfulwebservices;
+
+
+//Configuration
+//Enable Swagger
+public class SwaggerConfig {
+    
+    //Bean - Docket
+    
+         //Swagger 2
+         //All the paths
+         //All the APIs
+
+}
