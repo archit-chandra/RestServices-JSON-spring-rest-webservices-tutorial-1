005f6bbd70d0f5bcd7c24c6da96cc786d49fb044
archit_chandra <send2archit.chandra@gmail.com>
2018-01-01 08:21:50 +0100

    consume, produces info

--- src/main/java/com/gemalto/com/restfulwebservices/SwaggerConfig.java
+++ src/main/java/com/gemalto/com/restfulwebservices/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package com.gemalto.com.restfulwebservices;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,13 +17,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-    private static final Contact DEFAULT_CONTACT = new Contact("Archit Chandra", "https://github.com/archit-chandra", "archit.chandra@gemalto.com");
+    private static final Contact DEFAULT_CONTACT = new Contact("Archit Chandra", "https://github.com/archit-chandra",
+            "archit.chandra@gemalto.com");
     private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title", "Awesome API Description", "1.0",
             "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(
+            Arrays.asList("application/json, application/xml"));
 
-    //Bean - Docket
+    // Bean - Docket
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO);
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
