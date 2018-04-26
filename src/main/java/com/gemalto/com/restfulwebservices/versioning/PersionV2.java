b8acdf41224eacf2c0f5ff0d9114c43cde6a3b78
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-04-26 23:38:43 +0200

    versioning : created model classes

---
+++ src/main/java/com/gemalto/com/restfulwebservices/versioning/PersionV2.java
@@ -0,0 +1,22 @@
+package com.gemalto.com.restfulwebservices.versioning;
+
+
+public class PersionV2 {
+
+    private Name name;
+
+    public PersionV2() {
+    }
+
+    public PersionV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+}
