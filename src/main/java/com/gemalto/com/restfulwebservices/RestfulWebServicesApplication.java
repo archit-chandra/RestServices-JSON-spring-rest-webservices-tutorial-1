e67efd734ed63bc76ae34222be0d3baceaa751f8
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-04-26 22:07:37 +0200

    Localization Improvement : avoid @RequestHeader in every method

--- src/main/java/com/gemalto/com/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/gemalto/com/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,7 +1,5 @@
 package com.gemalto.com.restfulwebservices;
 
-import java.util.Locale;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
@@ -10,27 +8,35 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServicesApplication.class, args);
-	}
-	
-	@Bean
-	public LocaleResolver localeResolver() {
-	    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-	    localeResolver.setDefaultLocale(Locale.US);
-	    return localeResolver;
-	}
-	
-	// To read messages from the appropriate message properties file,
-	// and customized them based on the input accept header
-	@Bean
-	public MessageSource messageSource() {
-	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-	    messageSource.setBasename("locale/messages");
-	    messageSource.setDefaultEncoding("UTF-8");
-	    return messageSource;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServicesApplication.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        /**
+         *  To improve the localization, such that every method
+         *  should not add @RequestHeader(name = "Accept-Language", required = false)
+         *  to every method
+         */
+        //AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    // To read messages from the appropriate message properties file,
+    // and customized them based on the input accept header
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("locale/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
--- src/main/java/com/gemalto/com/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/gemalto/com/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,7 +1,5 @@
 package com.gemalto.com.restfulwebservices.helloworld;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,42 +7,56 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 //Controller
 @RestController
 public class HelloWorldController {
-    
+
     @Autowired
     private MessageSource messageSource;
-    
+
 //    @RequestMapping(method=RequestMethod.GET, path="/hello-world")
 //    public String helloWorld() {
 //        return "hello-world";
 //    }
-    
-    @GetMapping(path="/hello-world")
+
+    @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
     }
-    
-    @GetMapping(path="/hello-world-bean")
+
+    @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
     }
-    
-    @GetMapping(path="/hello-world-bean/path-variable/{name}")
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
-    
+
     //Localization => LocaleResolver (Default:Locale.US)
     //             => ResourceBundleResource
     // Usage:
     //Autowire MessageSource
     //@RequestHeader(value="Accept-Language, required=false) Locale locale
     //MessageSource.getMessage("helloWorld.message", null, locale)
-    @GetMapping(path="/hello-world-localized")
-    public String helloWorldLocalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+    @GetMapping(path = "/hello-world-localized")
+    public String helloWorldLocalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
         /*return "Good Morning"; // "Bonjour"*/
         return messageSource.getMessage("good.morning.message", null, locale);
     }
+
+    /**
+     *  To improve the localization, such that every method
+     *  should not add @RequestHeader(name = "Accept-Language", required = false)
+     *  to every method
+     */
+    /*@GetMapping(path = "/hello-world-localized")
+    public String helloWorldLocalized(@RequestHeader() {
+        *//*return "Good Morning"; // "Bonjour"*//*
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+    }*/
+
 }
