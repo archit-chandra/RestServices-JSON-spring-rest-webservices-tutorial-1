5029233bd7d326e2d351e69764592934b92d128b
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-04-26 23:50:08 +0200

    versioning : method 2

--- src/main/java/com/gemalto/com/restfulwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/gemalto/com/restfulwebservices/versioning/PersonVersioningController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
-    @GetMapping("/v1/person")
+    //@GetMapping("/v1/person")
+    @GetMapping(value = "/person/param", params = "version=1")
     public PersonV1 personV1() {
         return new PersonV1("Bob Charley");
     }
 
-    @GetMapping("/v2/person")
+    //@GetMapping("/v2/person")
+    @GetMapping(value = "/person/param", params = "version=2")
     public PersionV2 persionV2() {
         return new PersionV2(new Name("Bob", "Charlie"));
     }
